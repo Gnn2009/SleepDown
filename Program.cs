85516259e1b7933b9ed7a4382e5f74a03d4b9d0c
GNN8 <gananin09@gmail.com>
2026-03-05 19:57:14 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Diagnostics;
+namespace SleepDown
+{
+	class program
+	{
+		static void Main()
+		{
+      Console.WriteLine("¿En cuanto tiempo quieres que se apague el dispositivo?")
+
+    
+		}
+	}
+}
