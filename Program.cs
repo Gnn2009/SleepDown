515c08b28b8e718740f9291a150148e648ecd703
Gnn8 <gananin09@gmail.com>
2026-03-16 12:59:27 -0600

    Fix windows shutdown commands

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace SleepDown
 
             Console.Clear();
             Console.WriteLine($"El dispositivo se apagara en {minutos} minutos");
-            Process.Start("shutdown", "+" + entrada);
+            Process.Start("shutdown", $"-s -t {minutos * 60}");
 
             string? cancelar;
             do
@@ -28,7 +28,7 @@ namespace SleepDown
                 Console.WriteLine("Si quieres cancelar escribe o pega -cancelar-");
                 cancelar = Console.ReadLine()?.ToLower();
             } while (cancelar != "cancelar");
-            Process.Start("shutdown", "-c");
+            Process.Start("shutdown", "-a");
         }
     }
 }
